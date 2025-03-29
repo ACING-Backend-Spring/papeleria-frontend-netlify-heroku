380f359c15af37d224df3b98948395d454eb6469
Jose Luis Puentes Alamos <67543258+joseluispuentes82@users.noreply.github.com>
2025-03-29 10:34:47 +0100

    Cambiando urlHost de Heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   appName: 'Papeleria PROD',
   production: true,
-  urlHost: 'https://mipapeleria-backend-5619d1b34878.herokuapp.com/api/'
+  urlHost: 'https://papeleria-e98fdbd9ac77.herokuapp.com/api/'
 };
